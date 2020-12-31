d709c80f17eaae4db1d3a20524a8d24b4feec8ac
Ryan Hartlage <ryanplusplus@gmail.com>
2020-12-31 15:00:20 -0500

    It's working! It's working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Adafruit_ADXL345_U.h>
 
 extern "C" {
 #include <stddef.h>
@@ -8,14 +9,30 @@ extern "C" {
 }
 
 static tiny_timer_group_t timer_group;
+static Adafruit_ADXL345_Unified sensor;
 
 void setup()
 {
+  Serial.begin(9600);
+
+  sensor.begin();
+
   tiny_timer_group_init(&timer_group, tiny_time_source_init());
   tiny_heartbeat_init(&timer_group, 1000);
 }
 
 void loop()
 {
+  sensors_event_t event;
+  sensor.getEvent(&event);
+
+  Serial.print("(");
+  Serial.print(String(event.acceleration.x) + ", ");
+  Serial.print(String(event.acceleration.y) + ", ");
+  Serial.print(String(event.acceleration.z));
+  Serial.println(")");
+
+  delay(100);
+
   tiny_timer_group_run(&timer_group);
 }
